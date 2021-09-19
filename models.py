585f959ce88772542e797ba6b5a83c186eec7da4
mdwecht <mdwecht@gmail.com>
2021-09-19 17:17:16 -0400

    add more Job fields

--- models.py
+++ models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    name = models.CharField(max_length=20)
-    state = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    state = models.CharField(max_length=40)
     sec_to_next = models.IntegerField(default=3600)
-    params_tb = models.CharField(max_length=20)
+    params_tb = models.CharField(max_length=40)
     params_id = models.IntegerField(default=0)
     ran_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name    
